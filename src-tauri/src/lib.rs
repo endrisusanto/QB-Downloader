bcc0c4bd94ede99b8591dff853c0f125636cb921
endri Susanto <endrisusantosekarang@gmail.com>
2026-06-29 09:11:45 +0700

    chore: auto commit before release

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,11 +9,10 @@ mod system_stats;
 use download_manager::DownloadManager;
 use qb_client::QbClient;
 use tauri::{
-    menu::{CheckMenuItemBuilder, Menu, MenuItem},
+    menu::{Menu, MenuItem},
     tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent},
     Manager, State, WindowEvent,
 };
-use tauri_plugin_autostart::ManagerExt;
 use tauri_plugin_dialog::DialogExt;
 use types::{BuildArtifactGroup, Credentials, DownloadRequest, QuickBuildConfig, TokenTestResult};
 
@@ -155,7 +154,6 @@ fn get_local_ipv4() -> Option<String> {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_autostart::Builder::default().build())
         .setup(|app| {
             let salt_path = app
                 .path()
@@ -165,48 +163,22 @@ pub fn run() {
             app.handle()
                 .plugin(tauri_plugin_stronghold::Builder::with_argon2(&salt_path).build())?;
 
-            let autostart_manager = app.autolaunch();
-            let is_autostart_enabled = autostart_manager.is_enabled().unwrap_or(false);
-
             let show = MenuItem::with_id(app, "show", "Show", true, None::<&str>)?;
-            let autostart_item = CheckMenuItemBuilder::new("Start on Windows Startup")
-                .id("toggle-autostart")
-                .checked(is_autostart_enabled)
-                .build(app)?;
             let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-            let menu = Menu::with_items(app, &[&show, &autostart_item, &quit])?;
+            let menu = Menu::with_items(app, &[&show, &quit])?;
             let icon = tauri::image::Image::from_bytes(include_bytes!("../icons/icon.png"))?;
             if let Some(window) = app.get_webview_window("main") {
                 let _ = window.set_icon(icon.clone());
             }
 
-            let autostart_item_clone = autostart_item.clone();
             TrayIconBuilder::with_id("main-tray")
                 .tooltip("QuickBuild Download Manager")
                 .icon(icon)
                 .menu(&menu)
                 .show_menu_on_left_click(false)
-                .on_menu_event(move |app, event| match event.id().as_ref() {
+                .on_menu_event(|app, event| match event.id().as_ref() {
                     "show" => show_main_window(app),
                     "quit" => app.exit(0),
-                    "toggle-autostart" => {
-                        let app_handle = app.clone();
-                        let autostart_item = autostart_item_clone.clone();
-                        let _ = app.run_on_main_thread(move || {
-                            if let Ok(is_checked) = autostart_item.is_checked() {
-                                let new_state = !is_checked;
-                                let manager = app_handle.autolaunch();
-                                let res = if new_state {
-                                    manager.enable()
-                                } else {
-                                    manager.disable()
-                                };
-                                if res.is_ok() {
-                                    let _ = autostart_item.set_checked(new_state);
-                                }
-                            }
-                        });
-                    }
                     _ => {}
                 })
                 .on_tray_icon_event(|tray, event| {
